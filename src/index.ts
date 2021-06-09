9720b1ce63b3e3ba44bb2c091b499c13e2a986d8
ASSIONGBON Modeste <mo.assiongbon@gmail.com>
2021-06-09 02:31:16 +0200

    - Add test. - Fix bug found during test.

--- src/index.ts
+++ src/index.ts
@@ -4,108 +4,162 @@ import * as fs from "fs";
 interface Config {
   path: string;
   languages: string[];
-  defaultLanguage: string;
+  defaultLanguage?: string;
 }
 
 const lib: {
   _config: Config;
   _langData: { [k: string]: { [k: string]: string } };
+  _fileLoaded: boolean;
 } = {
   _config: null,
   _langData: null,
+  _fileLoaded: false,
 };
 
 export const setUp = (config: Config) => {
-  const { path, languages, defaultLanguage } = config;
-  lib._config = { path, languages, defaultLanguage };
+  // validate config
+  if (!config) {
+    throw new Error(
+      `The "setUp" function must be called with a parameter representing the configuration. The configuration must be an object with "path", "languages" and "defaultLanguage" attributes.`
+    );
+  }
+  if (!(typeof config === "object")) {
+    throw new Error(
+      `Invalid config ! The config must be an object with "path", "languages" and "defaultLanguage" attributes.`
+    );
+  }
+
+  if (!("path" in config) || !("languages" in config)) {
+    throw new Error(
+      `Invalid config ! The config must be an object with "path", "languages" and "defaultLanguage" attributes.`
+    );
+  }
+  const { path: i18nFilesDirpath, languages, defaultLanguage } = config;
+
+  validateI18nFilesDirPath(i18nFilesDirpath);
+
+  validateLanguages(languages);
+
+  validateDefaultLangage(defaultLanguage, languages);
+
+  lib._config = { path: i18nFilesDirpath, languages, defaultLanguage };
 };
-export const load = () => {
-  if (!lib._config) {
-    throw new Error(
-      "Not config found please call the setUp function to set the config before calling the load function"
-    );
-  }
 
-  if (!(typeof lib._config === "object")) {
-    throw new Error(
-      "Invalid config, the confi must be an object with 'path', 'languages' and 'defaultLanguage' attributes"
-    );
-  }
-
-  const { path: i18nFilesDirpath, languages, defaultLanguage } = lib._config;
+function validateI18nFilesDirPath(i18nFilesDirpath: string) {
   if (!i18nFilesDirpath) {
-    throw new Error("The path is required");
+    throw new Error(
+      `The "path" to the translation files directory is required.`
+    );
   } else {
     try {
       if (!fs.statSync(i18nFilesDirpath).isDirectory()) {
-        throw new Error("The path must be a path to a directory");
+        throw new Error(`"${i18nFilesDirpath}" is not a directory.`);
       }
     } catch (error) {
-      console.log("file or directory does not exist");
+      console.log(
+        `The file or directory "${i18nFilesDirpath}" does not exist.`
+      );
       throw error;
     }
   }
+}
 
+function validateLanguages(languages: string[]) {
   if (!languages) {
-    throw new Error("The languages is required");
+    throw new Error(`The "languages" is required`);
+  } else if (!Array.isArray(languages)) {
+    throw new Error(`The "languages" must be an array.`);
   } else if (!languages.length) {
     throw new Error(
-      "The languages can't be an empty array. You must specify at least one language."
+      `The "languages" can not be an empty array. You must specify at least one language.`
     );
   } else {
     const containsNonString = languages.some(
       (langKey) => typeof langKey !== "string"
     );
     if (containsNonString) {
-      throw new Error("The languages must be an array of string");
+      throw new Error(`The "languages" must be an array of string.`);
     }
   }
+}
 
+function validateDefaultLangage(defaultLanguage: string, languages: string[]) {
   if (defaultLanguage) {
     if (typeof defaultLanguage !== "string") {
-      throw new Error("The defaultLanguage must be a string");
+      throw new Error(`The "defaultLanguage" must be a string.`);
     } else if (!languages.includes(defaultLanguage)) {
       throw new Error(
-        "The defaultLanguage must take of the following values : ['en']"
+        `The "defaultLanguage" must take one of the following values : "${languages.join(
+          '", "'
+        )}".`
       );
     }
-  } else {
-    // use the first lanaguage as default if not specified
-    lib._config.defaultLanguage = lib._config.languages[0];
+  }
+}
+
+export const load = () => {
+  // validate config
+  if (!lib._config) {
+    throw new Error(
+      `No config found ! Please call the "setUp" function to set the config before calling the "load" function.`
+    );
   }
 
+  const { path: i18nFilesDirpath, languages } = lib._config;
   const translationsInfos = languages.map((langkey) => ({
     langkey,
     path: path.join(i18nFilesDirpath, `${langkey}.json`),
   }));
   // check that all the json languages files exist
   for (let translationInfo of translationsInfos) {
+    let fileStat;
     try {
-      if (!fs.statSync(translationInfo.path).isFile()) {
-        throw new Error("fr.json is not a file");
-      }
+      fileStat = fs.statSync(translationInfo.path);
     } catch (error) {
-      throw new Error("The file fr.json doesn't exist");
+      throw new Error(`The file "${translationInfo.path}" doesn't exist.`);
+    }
+    if (!fileStat.isFile()) {
+      throw new Error(
+        `The path "${translationInfo.path}" is not a valid file path.`
+      );
     }
   }
 
   lib._langData = {};
   // load each files content
   translationsInfos.forEach((info) => {
+    // if the content is not already loaded
     if (!(info.langkey in lib._langData)) {
       const rawData = fs.readFileSync(info.path);
       try {
         const langData = JSON.parse(rawData.toString());
         lib._langData[info.langkey] = langData;
-        console.log("File en.json loaded");
+        console.log(`File "${info.langkey}.json" loaded.`);
       } catch (error) {
-        console.log("An error happened when parsing the file en.json.");
+        console.log(
+          `An error happened when parsing the file ${info.langkey}.json (${info.path}).`
+        );
         throw new Error(error);
       }
     }
   });
-  console.log("Language files successfully loaded.");
+  lib._fileLoaded = true;
+  // use the first language default if not specified
+  if (!lib._config.defaultLanguage) {
+    lib._config.defaultLanguage = lib._config.languages[0];
+    console.log(
+      `No default language specified. The default language will be "${languages[0]}"`
+    );
+  }
+
+  console.log("Language files loaded successfully.");
+  console.log(
+    `Available Languages : ["${lib._config.languages.join('", "')}"].`
+  );
+  console.log(`Default Language : "${lib._config.defaultLanguage}".`);
 };
+
 export const i18n = (
   key: string,
   lang?: string,
@@ -113,7 +167,12 @@ export const i18n = (
 ) => {
   if (!lib._config) {
     throw new Error(
-      "Not config found please call the setUp function to set the config before calling the i18n function"
+      `No config found ! Please call the "setUp" function to set the config before calling the "i18n" function.`
+    );
+  }
+  if (!lib._fileLoaded) {
+    throw new Error(
+      `Translation files are not yet loaded. Please call the "load" function to load the translation files before calling the "i18n" function.`
     );
   }
 
@@ -121,12 +180,13 @@ export const i18n = (
     !lang || (lang && !lib._config.languages.includes(lang))
       ? lib._config.defaultLanguage
       : lang;
-
   let translatedString = lib._langData[i18nLang][key];
   if (translatedString) {
     if (props) {
       if (typeof props !== "object") {
-        throw new Error("The prop must be an object (i.e key-value pair)");
+        throw new Error(
+          `The third parameter of the "i18n" function must be an object (i.e a key-value pair).`
+        );
       }
 
       for (let key in props) {
@@ -142,14 +202,27 @@ export const i18n = (
 };
 
 export const setDefaultLang = (lang: string) => {
+  if (!lib._config) {
+    throw new Error(
+      `No config found ! Please call the "setUp" function to set the config before calling the "setDefaultLang" function.`
+    );
+  }
+  if (!lib._fileLoaded) {
+    throw new Error(
+      `Translation files are not yet loaded. Please call the "load" function to load the translation files before calling the "setDefaultLang" function.`
+    );
+  }
+
   if (!lang) {
-    throw new Error("The new defaultlang is required");
+    throw new Error("The value of the new default language is required.");
   }
   if (typeof lang !== "string") {
-    throw new Error("The  new defaultlang must be a string");
+    throw new Error("The value of the new default language must be a string.");
   } else if (!lib._config.languages.includes(lang)) {
     throw new Error(
-      "The lang is invalid. The lang must take of the following values : ['en']"
+      `The new default language is invalid. The new default language must take one of the following values : "${lib._config.languages.join(
+        '", "'
+      )}".`
     );
   }
   lib._config.defaultLanguage = lang;
---
+++ tests/index.spec.ts
@@ -0,0 +1,369 @@
+describe("", () => {
+  let i18nFilesDirPath: string;
+  let langFilePath: string = null;
+  let invalidPath: string = null;
+  let path: any;
+
+  let setUp: (config: any) => void;
+  let load: () => void;
+  let i18n: (
+    key: string,
+    lang?: string,
+    props?: {
+      [k: string]: any;
+    }
+  ) => string;
+
+  let setDefaultLang: (lang: string) => void;
+
+  beforeEach(() => {
+    return Promise.all([import("path"), import("../src")]).then(
+      ([pathModule, libModule]) => {
+        path = pathModule;
+        setUp = libModule.setUp;
+        load = libModule.load;
+        i18n = libModule.i18n;
+        setDefaultLang = libModule.setDefaultLang;
+
+        i18nFilesDirPath = path.join(__dirname, "i18n");
+        langFilePath = path.join(i18nFilesDirPath, "en.json");
+        invalidPath = path.join(i18nFilesDirPath, "invalid");
+      }
+    );
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+  describe('The "setUp" function', () => {
+    describe("should throw an error", () => {
+      it("when called with no parameter or with a parameter that is falsy", () => {
+        const expectedErrMsg =
+          'The "setUp" function must be called with a parameter representing the configuration. The configuration must be an object with "path", "languages" and "defaultLanguage" attributes.';
+        // @ts-ignore
+        expect(() => setUp()).toThrow(expectedErrMsg);
+        expect(() => setUp(undefined)).toThrow(expectedErrMsg);
+        expect(() => setUp(null)).toThrow(expectedErrMsg);
+      });
+
+      it("when called with a parameter that is not an object", () => {
+        const expectedErrMsg =
+          'Invalid config ! The config must be an object with "path", "languages" and "defaultLanguage" attributes.';
+        // @ts-ignore
+        expect(() => setUp("22")).toThrow(expectedErrMsg);
+        // @ts-ignore
+        expect(() => setUp(22)).toThrow(expectedErrMsg);
+      });
+
+      it('when called with an object that does not contain the "path", "languages" and "defaultLanguage" attribute', () => {
+        const expectedErrMsg =
+          'Invalid config ! The config must be an object with "path", "languages" and "defaultLanguage" attributes.';
+        // @ts-ignore
+        expect(() => setUp({})).toThrow(expectedErrMsg);
+        // @ts-ignore
+        expect(() => setUp({ path: "" })).toThrow(expectedErrMsg);
+        expect(() =>
+          setUp({ path: i18nFilesDirPath, languages: ["en"] })
+        ).not.toThrow(expectedErrMsg);
+      });
+      it("when the path to the folder containing the translation files is not specified", () => {
+        const expectedErrMsg =
+          'The "path" to the translation files directory is required.';
+        expect(() => setUp({ path: null, languages: ["en"] })).toThrow(
+          expectedErrMsg
+        );
+      });
+      it('when the "path" is not a directory', () => {
+        const expectedErrMsg = `"${langFilePath}" is not a directory.`;
+        expect(() => setUp({ path: langFilePath, languages: ["en"] })).toThrow(
+          expectedErrMsg
+        );
+      });
+
+      it('when the "path" is not a valid path', () => {
+        expect(() => setUp({ path: invalidPath, languages: ["en"] })).toThrow();
+      });
+
+      it('when the "languages" is not specified in the config', () => {
+        const expectedErrMsg = 'The "languages" is required';
+        expect(() =>
+          setUp({ path: i18nFilesDirPath, languages: null })
+        ).toThrow(expectedErrMsg);
+      });
+
+      it('when the value of "languages" is not an array', () => {
+        const expectedErrMsg = 'The "languages" must be an array.';
+        expect(() => {
+          //@ts-ignore
+          setUp({ path: i18nFilesDirPath, languages: 2 });
+        }).toThrow(expectedErrMsg);
+      });
+
+      it('when the value of "languages" is an empty array', () => {
+        const expectedErrMsg =
+          'The "languages" can not be an empty array. You must specify at least one language.';
+        expect(() => setUp({ path: i18nFilesDirPath, languages: [] })).toThrow(
+          expectedErrMsg
+        );
+      });
+
+      it('when the value of "languages" is not an array of string', () => {
+        const expectedErrMsg = 'The "languages" must be an array of string.';
+        expect(() => {
+          //@ts-ignore
+          setUp({ path: i18nFilesDirPath, languages: ["en", 2] });
+        }).toThrow(expectedErrMsg);
+      });
+
+      it('when the "defaultLanguage" is not a string', () => {
+        const expectedErrMsg = 'The "defaultLanguage" must be a string.';
+        expect(() => {
+          setUp({
+            path: i18nFilesDirPath,
+            languages: ["en", "fr"],
+            //@ts-ignore
+            defaultLanguage: 2,
+          });
+        }).toThrow(expectedErrMsg);
+      });
+
+      it('when the "defaultLanguage" is not a string in the "languages" array', () => {
+        const languages = ["en", "fr"];
+        const expectedErrMsg = `The "defaultLanguage" must take one of the following values : "${languages.join(
+          '", "'
+        )}".`;
+        expect(() => {
+          setUp({
+            path: i18nFilesDirPath,
+            languages,
+            defaultLanguage: "es",
+          });
+        }).toThrow(expectedErrMsg);
+      });
+      /* 
+
+
+
+
+
+
+
+*/
+    });
+    // it("", () => {});
+    /* it("", () => {});
+    it("", () => {});
+
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {});
+    it("", () => {}); */
+  });
+
+  describe('The "load" function', () => {
+    describe("should throw an error", () => {
+      it('when it is not called after the "setUp" function', () => {
+        const expectedErrMsg =
+          'No config found ! Please call the "setUp" function to set the config before calling the "load" function.';
+        expect(() => load()).toThrow(expectedErrMsg);
+      });
+
+      it('when a json file corresponding to a language specified in the "languages" attribute of the config is not found', () => {
+        const filePath = path.join(i18nFilesDirPath, "es.json");
+        const expectedErrMsg = `The file "${filePath}" doesn't exist.`;
+        setUp({ path: i18nFilesDirPath, languages: ["en", "es", "fr"] });
+        expect(() => load()).toThrow(expectedErrMsg);
+      });
+
+      it('when the path to a json file corresponding to a language specified in the "languages" attribute of the config is not a file path', () => {
+        const expectedErrMsg = `The path "${path.join(
+          i18nFilesDirPath,
+          "it.json"
+        )}" is not a valid file path.`;
+        setUp({ path: i18nFilesDirPath, languages: ["en", "it", "fr"] });
+        expect(() => load()).toThrow(expectedErrMsg);
+      });
+
+      it("when a json file corresponding to a language does not contains a valid json", () => {
+        const filePath = path.join(i18nFilesDirPath, "ch.json");
+
+        const expectedErrMsg = `An error happened when parsing the file ${"ch"}.json (${filePath}).`;
+
+        setUp({ path: i18nFilesDirPath, languages: ["en", "ch", "fr"] });
+        // const spy = jest.spyOn(console, "log");
+        // expect(spy).toHaveBeenCalledWith(expectedErrMsg);
+        expect(() => load()).toThrow("SyntaxError:");
+      });
+    });
+  });
+
+  describe('The "setDefaultLang" function', () => {
+    describe("should throw an error", () => {
+      it('when it is not called after the "setUp" function', () => {
+        const expectedErrMsg =
+          'No config found ! Please call the "setUp" function to set the config before calling the "setDefaultLang" function.';
+        expect(() => setDefaultLang("fr")).toThrow(expectedErrMsg);
+      });
+
+      it('when it is not called after the "setUp" and "load" functions called', () => {
+        setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+
+        const expectedErrMsg =
+          'Translation files are not yet loaded. Please call the "load" function to load the translation files before calling the "setDefaultLang" function.';
+        expect(() => setDefaultLang("fr")).toThrow(expectedErrMsg);
+      });
+
+      it("when called with no parameter or with a parameter that is falsy", () => {
+        setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+        load();
+        const expectedErrMsg =
+          "The value of the new default language is required.";
+        // @ts-ignore
+        expect(() => setDefaultLang()).toThrow(expectedErrMsg);
+        expect(() => setDefaultLang(undefined)).toThrow(expectedErrMsg);
+        expect(() => setDefaultLang(null)).toThrow(expectedErrMsg);
+      });
+
+      it("when called with a parameter that is not a string", () => {
+        setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+        load();
+        const expectedErrMsg =
+          "The value of the new default language must be a string.";
+        // @ts-ignore
+        expect(() => setDefaultLang(22)).toThrow(expectedErrMsg);
+        // @ts-ignore
+        expect(() => setDefaultLang(true)).toThrow(expectedErrMsg);
+      });
+
+      it('called with a value that is not in the "languages" array of the config', () => {
+        const languages = ["en", "fr"];
+        setUp({ path: i18nFilesDirPath, languages });
+        load();
+        const expectedErrMsg = `The new default language is invalid. The new default language must take one of the following values : "${languages.join(
+          '", "'
+        )}".`;
+        expect(() => setDefaultLang("it")).toThrow(expectedErrMsg);
+        expect(() => setDefaultLang("fr")).not.toThrow(expectedErrMsg);
+        expect(() => setDefaultLang("en")).not.toThrow(expectedErrMsg);
+      });
+    });
+  });
+
+  describe('The "i18n" function', () => {
+    describe("should throw an error", () => {
+      it('when it is not called after the "setUp" function', () => {
+        const expectedErrMsg =
+          'No config found ! Please call the "setUp" function to set the config before calling the "i18n" function.';
+        expect(() => i18n("fr")).toThrow(expectedErrMsg);
+      });
+
+      it('when it is not called after the "setUp" and "load" functions called', () => {
+        setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+
+        const expectedErrMsg =
+          'Translation files are not yet loaded. Please call the "load" function to load the translation files before calling the "i18n" function.';
+        expect(() => i18n("fr")).toThrow(expectedErrMsg);
+      });
+
+      it('when the third parameter of the "i18n" is defined but is not an object', () => {
+        setUp({
+          path: i18nFilesDirPath,
+          languages: ["en", "fr"],
+          defaultLanguage: "fr",
+        });
+        load();
+        const expectedErrMsg =
+          'The third parameter of the "i18n" function must be an object (i.e a key-value pair).';
+        // @ts-ignore
+        expect(() => i18n("i_x_years", null, 2)).toThrow(expectedErrMsg);
+      });
+    });
+
+    it("Should not throw error when the config is valid", () => {
+      setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+      load();
+      expect(() => i18n("username")).not.toThrow();
+    });
+
+    it("Should return the right translation", () => {
+      setUp({
+        path: i18nFilesDirPath,
+        languages: ["en", "fr"],
+      });
+      load();
+      expect(i18n("username", "fr")).toBe("Nom d'utilisateur");
+      expect(i18n("username", "en")).toBe("Username");
+    });
+
+    it("Should return the translation key when no corresponding tranlation is found", () => {
+      setUp({
+        path: i18nFilesDirPath,
+        languages: ["en", "fr"],
+      });
+      load();
+      expect(i18n("hello_world", "fr")).toBe("hello_world");
+      expect(i18n("hello_world", "en")).toBe("hello_world");
+    });
+
+    it("Should return the right translation with the right string interpolation", () => {
+      const age = 12;
+      const name = "Ghost";
+
+      const str1Fr = `J'ai ${age} ans.`;
+      const str1En = `I am ${age} old.`;
+
+      const str2Fr = `age: ${age} Nom: ${name} age: ${age}.`;
+      const str2En = `age: ${age} Name: ${name} age: ${age}.`;
+
+      setUp({ path: i18nFilesDirPath, languages: ["en", "fr"] });
+      load();
+
+      expect(i18n("i_x_years", "fr", { age })).toBe(str1Fr);
+      expect(i18n("i_x_years", "en", { age })).toBe(str1En);
+
+      expect(i18n("test", "fr", { age, name })).toBe(str2Fr);
+      expect(i18n("test", "en", { age, name })).toBe(str2En);
+    });
+
+    it('Should translate to the "defaultLanguage" when no language is specified when calling the "i18n" function', () => {
+      const age = 12;
+      const strFr = `J'ai ${age} ans.`;
+
+      setUp({
+        path: i18nFilesDirPath,
+        languages: ["en", "fr"],
+        defaultLanguage: "fr",
+      });
+      load();
+      expect(i18n("i_x_years", null, { age })).toBe(strFr);
+      expect(i18n("username", null)).toBe("Nom d'utilisateur");
+    });
+
+    it('Should update the default language when the "setDefaultLang" function is called', () => {
+      const age = 12;
+      const strFr = `J'ai ${age} ans.`;
+      const strEn = `I am ${age} old.`;
+
+      setUp({
+        path: i18nFilesDirPath,
+        languages: ["en", "fr"],
+        defaultLanguage: "fr",
+      });
+      load();
+
+      setDefaultLang("fr");
+      expect(i18n("i_x_years", null, { age })).toBe(strFr);
+      expect(i18n("username", null)).toBe("Nom d'utilisateur");
+
+      setDefaultLang("en");
+      expect(i18n("i_x_years", null, { age })).toBe(strEn);
+      expect(i18n("username", null)).toBe("Username");
+    });
+  });
+});
